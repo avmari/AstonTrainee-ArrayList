214865606672a48c81a46ada5c1631e2d9c34b8b
avmari <mariya.avdeeva251001@gmail.com>
2023-09-12 11:19:53 +0400

    Custom ArrayList + junit tests on its methods

--- src/main/java/collections/ArrayList.java
+++ src/main/java/collections/ArrayList.java
@@ -1,2 +1,149 @@
-package collections;public class ArrayList {
+package collections;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Custom ArrayList with basic methods to add, remove, get and sort elements of ArrayList
+ * Permits all elements, including null.
+ *
+ * @param <T> determines the type of elements stored in ArrayList
+ */
+public class ArrayList<T> {
+    private T[] values;
+    private int size;
+    private static final int INITIAL_CAPACITY = 16;
+
+    public ArrayList() {
+        values = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     * appends new element to the end of ArrayList
+     *
+     * @param value - value to be added to ArrayList
+     */
+    public boolean add(T value) {
+        add(value, size);
+        return true;
+    }
+
+    /**
+     * adds specified value at specified index to ArrayList
+     *
+     * @param value - value to be added to ArrayList
+     * @param index - index at which specified value should be added
+     * @throws IndexOutOfBoundsException if (index < 0 || index > size)
+     */
+    public void add(T value, int index) {
+        checkIndexForAdd(index);
+        if (size == values.length)
+            grow();
+        System.arraycopy(values, index, values, index + 1, size - index);
+        values[index] = value;
+        size++;
+    }
+
+    /**
+     * returns element of ArrayList at specified index
+     *
+     * @param index - index of element to return
+     * @return element of ArrayList at specified index
+     * @throws IndexOutOfBoundsException if (index < 0 || index >= size)
+     */
+    public T get(int index) {
+        checkIndex(index);
+        return values[index];
+    }
+
+    /**
+     * removes element of ArrayList at specified index
+     *
+     * @param index - index of element to remove
+     * @throws IndexOutOfBoundsException if (index < 0 || index >= size)
+     */
+    public T remove(int index) {
+        checkIndex(index);
+        T removedValue = values[index];
+        if (index != size - 1)
+            System.arraycopy(values, index + 1, values, index, size - index - 1);
+        values[size--] = null;
+        return removedValue;
+    }
+
+    /**
+     * removes all elements from ArrayList
+     */
+    public void clearAll() {
+        size = 0;
+        for (T o : values)
+            o = null;
+    }
+
+    /**
+     * sorts ArrayList with QuickSort algorithm, uses comparator for comparing elements of ArrayList
+     *
+     * @param comparator used for comparing elements of ArrayList
+     */
+    public void sort(Comparator<T> comparator) {
+        quickSort(values, 0, size - 1, comparator);
+    }
+
+    /**
+     * replaces element of ArrayList at specified index with specified value
+     *
+     * @param index - index of element that should be replaced
+     * @param value - value with which element is replaced
+     * @throws IndexOutOfBoundsException if (index < 0 || index >= size)
+     */
+    public void replace(int index, T value) {
+        checkIndex(index);
+        values[index] = value;
+    }
+
+    private void grow() {
+        values = Arrays.copyOf(values, values.length << 1);
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+    }
+
+    private void quickSort(T[] values, int low, int high, Comparator<T> comparator) {
+        if (low >= high)
+            return;
+
+        T pivot = get(high);
+
+        int i = low - 1;
+        for (int j = low; j < high; ++j) {
+
+            if (comparator.compare(values[j], pivot) < 0) {
+                ++i;
+
+                T temp = values[j];
+                values[j] = values[i];
+                values[i] = temp;
+            }
+        }
+
+        values[high] = values[i + 1];
+        values[i + 1] = pivot;
+
+        quickSort(values, low, i, comparator);
+        quickSort(values, i + 2, high, comparator);
+    }
+
 }
--- src/test/java/collections/ArrayListTest.java
+++ src/test/java/collections/ArrayListTest.java
@@ -1,4 +1,174 @@
 package collections;
 
-public class ArrayListTest {
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTest {
+
+    private final ArrayList<Integer> arrayList = new ArrayList<>();
+
+    private final int[] integers = {5, 4, 1, 2, 7};
+    private final int size = 5;
+
+    @BeforeEach
+    void init() {
+        for (int i : integers)
+            arrayList.add(i);
+    }
+
+    @Test
+    void add() {
+        int valueToAdd = 100;
+
+        arrayList.add(valueToAdd);
+
+        int[] newIntegers = {5, 4, 1, 2, 7, valueToAdd};
+
+        for (int i = 0; i < size + 1; ++i) {
+            assertEquals(arrayList.get(i), newIntegers[i]);
+        }
+        assertEquals(arrayList.size(), size + 1);
+    }
+
+    @Test
+    void addIfCapacityEqualsSize() {
+        int valueToAdd = 100;
+        int[] newElements = {1, 2, 3, 4, 1, 3, 1, 2, 2, 1, 1};
+        for (int i : newElements)
+            arrayList.add(i);
+
+        arrayList.add(valueToAdd);
+
+        assertEquals(arrayList.get(arrayList.size() - 1), valueToAdd);
+        assertEquals(arrayList.size(), size + newElements.length + 1);
+    }
+
+    @Test
+    void addIfArrayListIsEmpty() {
+        arrayList.clearAll();
+        int valueToAdd = 100;
+
+        arrayList.add(valueToAdd);
+
+        assertEquals(arrayList.get(arrayList.size() - 1), valueToAdd);
+        assertEquals(arrayList.size(), 1);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getIndexAndValueToAdd")
+    void addWithIndex(int index, int value, int[] result) {
+        arrayList.add(value, index);
+
+        for (int i = 0; i < result.length; ++i) {
+            assertEquals(result[i], arrayList.get(i));
+        }
+        assertEquals(arrayList.size(), result.length);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getWrongIndexAndValue")
+    void addWithWrongIndex(int index, Integer value) {
+        assertThrows(IndexOutOfBoundsException.class, () -> arrayList.add(value, index));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 2, 4})
+    void get(int index) {
+        assertEquals(arrayList.get(index), integers[index]);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, size})
+    void getWithIndexOutOfBounds(int index) {
+        assertThrows(IndexOutOfBoundsException.class, () -> arrayList.get(index));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getResultAndIndexToRemove")
+    void remove(int index, int[] result) {
+        arrayList.remove(index);
+
+        for (int i = 0; i < result.length; ++i) {
+            assertEquals(result[i], arrayList.get(i));
+        }
+        assertEquals(arrayList.size(), result.length);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, size})
+    void removeWithIndexOutOfBounds(int index) {
+        assertThrows(IndexOutOfBoundsException.class, () -> arrayList.remove(index));
+    }
+
+    @Test
+    void clearAll() {
+        arrayList.clearAll();
+
+        assertEquals(arrayList.size(), 0);
+        assertThrows(IndexOutOfBoundsException.class, () -> arrayList.get(0));
+    }
+
+    @Test
+    void sort() {
+        arrayList.sort(Comparator.naturalOrder());
+
+        int[] sortedIntegers = Arrays.copyOf(integers, integers.length);
+        Arrays.sort(sortedIntegers);
+
+        for (int i = 0; i < sortedIntegers.length; ++i) {
+            assertEquals(arrayList.get(i), sortedIntegers[i]);
+        }
+    }
+
+    @Test
+    void replace() {
+        int newValue = 100;
+        int index = 2;
+
+        arrayList.replace(index, newValue);
+
+        assertEquals(arrayList.get(index), newValue);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getWrongIndexAndValue")
+    void replaceWithIndexOutOfBounds(int index, Integer value) {
+        assertThrows(IndexOutOfBoundsException.class, () -> arrayList.replace(index, value));
+    }
+
+    private static Stream<Arguments> getWrongIndexAndValue() {
+        return Stream.of(
+                Arguments.of(-1, 1),
+                Arguments.of(100, 1)
+        );
+    }
+
+    private static Stream<Arguments> getResultAndIndexToRemove() {
+        return Stream.of(
+                Arguments.of(0, new int[]{4, 1, 2, 7}),
+                Arguments.of(1, new int[]{5, 1, 2, 7}),
+                Arguments.of(4, new int[]{5, 4, 1, 2})
+        );
+    }
+
+    private static Stream<Arguments> getIndexAndValueToAdd() {
+        return Stream.of(
+                Arguments.of(0, 9, new int[]{9, 5, 4, 1, 2, 7}),
+                Arguments.of(1, 2, new int[]{5, 2, 4, 1, 2, 7}),
+                Arguments.of(4, 8, new int[]{5, 4, 1, 2, 8, 7}),
+                Arguments.of(5, 10, new int[]{5, 4, 1, 2, 7, 10})
+        );
+    }
+
 }
